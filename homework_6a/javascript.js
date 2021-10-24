91d73dd551e9ef422c426b3bdb0a5f93073524fd
pvegired <pvegired@andrew.cmu.edu>
2021-10-24 18:57:08 -0400

    separate javascript file

---
+++ homework_6a/javascript.js
@@ -0,0 +1,124 @@
+function afterschoolspecial() {
+            var mainimage = document.getElementById('mainimage');
+            var sideimage1 = document.getElementById('sideimage1');
+            var sideimage2 = document.getElementById('sideimage2');
+            var sideimage3 = document.getElementById('sideimage3');
+            var sideimage4 = document.getElementById('sideimage4');
+            var sideimage5 = document.getElementById('sideimage5');
+
+
+            if (mainimage.src.match("images/h1.png")) {
+                mainimage.src = "images/blueimage.jpg";
+            }
+            if (sideimage1.src.match("images/h2.png")) {
+                sideimage1.src = "images/blueimage2.jpg";
+            }
+            if (sideimage2.src.match("images/h3.png")) {
+                sideimage2.src = "images/blueimage3.jpg";
+            }
+            if (sideimage3.src.match("images/h1.png")) {
+                sideimage3.src = "images/blueimage.jpg";
+            }
+            if (sideimage4.src.match("images/h2.png")) {
+                sideimage4.src = "images/blueimage2.jpg";
+            }
+            if (sideimage5.src.match("images/h3.png")) {
+                sideimage5.src = "images/blueimage3.jpg";
+            }
+            else {
+                console.log("nothing happened");
+            }
+        }
+
+    function morninghaze() {
+            var mainimage = document.getElementById('mainimage');
+            var sideimage1 = document.getElementById('sideimage1');
+            var sideimage2 = document.getElementById('sideimage2');
+            var sideimage3 = document.getElementById('sideimage3');
+            var sideimage4 = document.getElementById('sideimage4');
+            var sideimage5 = document.getElementById('sideimage5');
+
+            if (mainimage.src.match("images/h1.png")) {
+                mainimage.src = "images/grey.jpg";
+            }
+            if (sideimage1.src.match("images/h2.png")) {
+                sideimage1.src = "images/grey2.jpg";
+            }
+            if (sideimage2.src.match("images/h3.png")) {
+                sideimage2.src = "images/grey3.jpg";
+            }
+            if (sideimage3.src.match("images/h1.png")) {
+                sideimage3.src = "images/grey.jpg";
+            }
+            if (sideimage4.src.match("images/h2.png")) {
+                sideimage4.src = "images/grey2.jpg";
+            }
+            if (sideimage5.src.match("images/h3.png")) {
+                sideimage5.src = "images/grey3.jpg";
+            }
+            else {
+                console.log("nothing happened");
+            }
+        }
+
+    function cozydenim() {
+            var mainimage = document.getElementById('mainimage');
+            var sideimage1 = document.getElementById('sideimage1');
+            var sideimage2 = document.getElementById('sideimage2');
+            var sideimage3 = document.getElementById('sideimage3');
+            var sideimage4 = document.getElementById('sideimage4');
+            var sideimage5 = document.getElementById('sideimage5');
+
+            if (mainimage.src.match("images/h1.png")) {
+                mainimage.src = "images/blueimage.jpg";
+            }
+            if (sideimage1.src.match("images/h2.png")) {
+                sideimage1.src = "images/blueimage2.jpg";
+            }
+            if (sideimage2.src.match("images/h3.png")) {
+                sideimage2.src = "images/blueimage3.jpg";
+            }
+            if (sideimage3.src.match("images/h1.png")) {
+                sideimage3.src = "images/blueimage.jpg";
+            }
+            if (sideimage4.src.match("images/h2.png")) {
+                sideimage4.src = "images/blueimage2.jpg";
+            }
+            if (sideimage5.src.match("images/h3.png")) {
+                sideimage5.src = "images/blueimage3.jpg";
+            }
+            else {
+                console.log("nothing happened");
+            }
+        }
+    
+    function rainyday() {
+            var mainimage = document.getElementById('mainimage');
+            var sideimage1 = document.getElementById('sideimage1');
+            var sideimage2 = document.getElementById('sideimage2');
+            var sideimage3 = document.getElementById('sideimage3');
+            var sideimage4 = document.getElementById('sideimage4');
+            var sideimage5 = document.getElementById('sideimage5');
+
+            if (mainimage.src.match("images/h1.png")) {
+                mainimage.src = "images/blueimage.jpg";
+            }
+            if (sideimage1.src.match("images/h2.png")) {
+                sideimage1.src = "images/blueimage2.jpg";
+            }
+            if (sideimage2.src.match("images/h3.png")) {
+                sideimage2.src = "images/blueimage3.jpg";
+            }
+            if (sideimage3.src.match("images/h1.png")) {
+                sideimage3.src = "images/blueimage.jpg";
+            }
+            if (sideimage4.src.match("images/h2.png")) {
+                sideimage4.src = "images/blueimage2.jpg";
+            }
+            if (sideimage5.src.match("images/h3.png")) {
+                sideimage5.src = "images/blueimage3.jpg";
+            }
+            else {
+                console.log("nothing happened");
+            }
+        }
